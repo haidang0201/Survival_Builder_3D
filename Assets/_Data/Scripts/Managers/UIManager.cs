764843f034b39f16e8fbc09b727d5ec3432ebc45
HAIDANG205 <l3eatcmnr@gmail.com>
2026-05-12 21:29:45 +0700

    fix_cam_build

--- Assets/_Data/Scripts/Managers/UIManager.cs
+++ Assets/_Data/Scripts/Managers/UIManager.cs
@@ -4,6 +4,19 @@ public class UIManager : Singleton<UIManager>
 {
     [SerializeField] private GameObject warningUI;
 
+    [SerializeField] private GameObject houseSelectionPanel;
+    [SerializeField] private GameObject workerStatusPanel;
+
+
+    void Start()
+    {
+        houseSelectionPanel.SetActive(true);
+        workerStatusPanel.SetActive(true);
+    }
+
+
+
+
     // Hiển thị cảnh báo trên giao diện
     public void ShowWarning(string message)
     {
@@ -26,14 +39,14 @@ public class UIManager : Singleton<UIManager>
     }
     public void OnClickSawmillButton()
     {
-        BuildingSystem.Ins.StartPlacing(BuildingType.ForestHut);
+        BuildingSystem.Ins.StartPlacing(BuildingType.Sawmill);
     }
     public void OnClickWarehouseButton()
     {
-        BuildingSystem.Ins.StartPlacing(BuildingType.ForestHut);
+        BuildingSystem.Ins.StartPlacing(BuildingType.Warehouse);
     }
     public void OnClickHouseBuilderButton()
     {
-        BuildingSystem.Ins.StartPlacing(BuildingType.ForestHut);
+        BuildingSystem.Ins.StartPlacing(BuildingType.HouseBuilder);
     }
 }
