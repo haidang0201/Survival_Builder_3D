ef1c9fd2a786c32947de05a2318b11e17b1da6a0
vuok-leuleu <vudaydu123@gmail.com>
2026-05-18 19:42:08 +0700

    di chuyển code
    di chuyển code
    setting lại các nút

--- Assets/_Data/Scripts/Managers/UIManager.cs
+++ Assets/_Data/Scripts/Managers/UIManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class UIManager : Singleton<UIManager>
 {
@@ -11,6 +13,19 @@ public class UIManager : Singleton<UIManager>
 
     [SerializeField] private GameObject houseSelectionPanel;
     [SerializeField] private GameObject workerStatusPanel;
+
+
+    //========================PauseMenu============================
+    [Header("Panels")]
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private GameObject settingsPanel;
+
+    [Header("Scene")]
+    [SerializeField] private string gameplaySceneName;
+
+    private bool isPaused = false;
+
+    //==============================================================
 
 
     void Start()
@@ -54,4 +69,74 @@ public class UIManager : Singleton<UIManager>
     {
         BuildingSystem.Ins.StartPlacing(BuildingType.House);
     }
+
+    //========================PauseMenu============================
+    void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            if (!isPaused)
+                PauseGame();
+            else
+                ResumeGame();
+        }
+    }
+
+    // ================= PAUSE =================
+
+    void PauseGame()
+    {
+        isPaused = true;
+
+        pausePanel.SetActive(true);
+        settingsPanel.SetActive(false);
+
+        Time.timeScale = 0f;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    void ResumeGame()
+    {
+        isPaused = false;
+
+        pausePanel.SetActive(false);
+        settingsPanel.SetActive(false);
+
+        Time.timeScale = 1f;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // ================= BUTTON =================
+
+    // Nút Continue
+    public void OnClickContinue()
+    {
+        ResumeGame();
+    }
+
+    // Nút mở Settings
+    public void OnClickOpenSettings()
+    {
+        pausePanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    // Nút back từ Settings về Pause
+    public void OnClickBackToPause()
+    {
+        settingsPanel.SetActive(false);
+        pausePanel.SetActive(true);
+    }
+
+    // Nút về House (load scene)
+    public void OnClickBackToHouse()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(gameplaySceneName);
+    }
+
 }
